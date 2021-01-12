3b9b6b1d8eb3a6a08b5cbaf3ffa64f922727d434
JunJG <JunJG@uci.edu>
2021-01-11 22:44:48 -0800

    Followed along with udemy course to add back-end to communicate with the front-end side

--- src/App.js
+++ src/App.js
@@ -34,8 +34,25 @@ class App extends React.Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        email: '',
+        id: '',
+        name: '',
+        entries: 0,
+        joined: ''
+      }
     };
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        email: data.email,
+        id: data.id,
+        name: data.name,
+        entries: data.entries,
+        joined: data.joined
+    }});
   }
 
   calculateFaceLocation = (data) => {
@@ -66,7 +83,21 @@ class App extends React.Component {
     // Bounding box is in response.outputs[0].data.regions[0].region_info.bounding_box
     // "a403429f2ddf4b49b307e318f00e528b" is face-det model
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => { this.displayFaceBox( this.calculateFaceLocation(response) ) })
+    .then(response => { 
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        }).then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count}));
+          });
+      }
+      this.displayFaceBox( this.calculateFaceLocation(response) ) 
+    })
     .catch(err => console.log(err)); 
   }
 
@@ -90,7 +121,7 @@ class App extends React.Component {
           ? 
           <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm 
               onInputChange={this.onInputChange} 
               onButtonSubmit={this.onButtonSubmit}
@@ -98,9 +129,9 @@ class App extends React.Component {
             <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
           </div>
           : ( this.state.route === 'signin' || this.state.route === 'signout' ?
-              <Signin onRouteChange={this.onRouteChange}/> 
+              <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser}/> 
               :
-              <Register onRouteChange={this.onRouteChange}/> 
+              <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/> 
             )
         }
       </div>
